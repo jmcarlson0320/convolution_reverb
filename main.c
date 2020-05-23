8d02c9cf8aa0e81fc0a197af75045bab23cd9b35
Joshua Carlson <jmc34@pdx.edu>
2020-05-23 16:48:11 -0700

    basic argument parsing

--- main.c
+++ main.c
@@ -2,19 +2,53 @@
 
 void usage()
 {
-    printf("usage: reverb audiofile.wav\n");
+    printf("usage: reverb [-oi] [outputfile|impulseresponse] inputfile\n"
+           "usage: reverb -l (list available impulse responses)\n");
 }
 
 int main(int argc, char *argv[])
 {
-    char *input_file;
-    char *output_file;
+    enum {
+        OUTPUT_FILE_SPECIFIED,
+        IR_SPECIFIED,
+        LIST_AVAILABLE_IR,
+        NUM_MODES
+    };
 
-    if (argc !=  2) {
-        usage();
-        return 1;
+    char *input_file = NULL;
+    char *output_file = NULL;
+    int index = 1;
+    int options[NUM_MODES] = {0};
+
+    while (index < argc && argv[index][0] == '-') {
+        switch (argv[index][1]) {
+            case 'o':
+                // set output file
+                if (++index < argc) {
+                    options[OUTPUT_FILE_SPECIFIED] = 1;
+                    output_file = argv[index];
+                }
+                break;
+            case 'i':
+                // set impulse response
+                break;
+            case 'l':
+                // print available inpulse responses
+                break;
+            default:
+                usage();
+                break;
+        }
+        index++;
     }
-    input_file = argv[1];
+
+    if (index < argc)
+        input_file = argv[index];
+    else
+        usage();
+
+    printf("input file: %s\n", input_file);
+    printf("output file: %s\n", output_file);
 
     return 0;
 }
